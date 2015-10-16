06a046d20a9e3eb7ac4e1f0eddde0ea44454a7ef
charissatolentino@gmail.com <hitobochi@Risa-Tolentinos-MacBook-Pro.local>
2015-10-16 16:15:19 -0500

    photogram initial commit

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,11 @@
 class Photo < ActiveRecord::Base
+  validates :caption, presence: true
+  validates :source, presence: true
+  validates :source, length: { maximum: 500 }
+  validate :source_must_be_an_image
+
+  def source_must_be_an_image
+    if source[-3..-1] != 'jpg' && source[-3..-1] != 'png'
+      errors.add(:image_format, 'must be jpg or png')
+    end
 end
